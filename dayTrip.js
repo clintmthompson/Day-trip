23e755e88950ed89a2a3887b8a7eadd6506bcd29
Mega Farts <clint.m.thompson@outlook.com>
2021-04-30 11:43:42 -0500

    Added a function to randomize the output of an array. Added 4 new arrays

---
+++ dayTrip.js
@@ -0,0 +1,15 @@
+"use strict"
+
+function randomizeArray(array){
+    let l = array.length;
+    let pos = Math.floor(Math.random() * array.length);
+    let roundPos = Math.round(pos);
+    let result = array[roundPos];
+    return(result);
+}
+
+let destinations = ["Spain", "Chile", "Poland", "Canada"];
+let restaurant = ["Chili's", "McDonalds", "Denny's", "Food from home"]
+let transport = ["You're walking, and you may have to swim", "Helicopter", "Airplane"]
+let entertainment = ["Mariachi Concert", "Drive-in Movie", "Comedy Show"]
+

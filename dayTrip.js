c491948427cf9c385ed765776257f52d302644a4
Mega Farts <clint.m.thompson@outlook.com>
2021-05-01 12:56:52 -0500

    Added variables for button click results. Linked output of randomize function to text boxes. Combined elements to display results in console on button push

--- dayTrip.js
+++ dayTrip.js
@@ -8,8 +8,17 @@ function randomizeArray(array){
     return(result);
 }
 
+
+
+
 let destinations = ["Spain", "Chile", "Poland", "Canada"];
 let restaurant = ["Chili's", "McDonalds", "Denny's", "Food from home"]
 let transport = ["You're walking, and you may have to swim", "Helicopter", "Airplane"]
 let entertainment = ["Mariachi Concert", "Drive-in Movie", "Comedy Show"]
 
+
+
+let dest = randomizeArray(destinations)
+let rest = randomizeArray(restaurant)
+let tran = randomizeArray(transport)
+let ente = randomizeArray(entertainment)
